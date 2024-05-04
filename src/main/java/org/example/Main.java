68079f658fd642127b5d1d980beb39a8dffae8fd
Kuba Krychowski <kubolot33123@gmail.com>
2024-05-04 08:46:41 +0200

    ZAD-4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ import company.beans.Person;
 import company.implementations.EmailMessenger;
 import company.utils.MathUtils;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         try {
@@ -22,5 +24,20 @@ public class Main {
 
         String mathResult = String.valueOf(MathUtils.add(2, 2));
         emailMessenger.sendMessage(mathResult);
+
+        Person[] people = new Person[5];
+        final int b = 10;
+        Random random = new Random();
+        for (Person person : people) {
+            try {
+                person = new Person("John Doe", 10 + random.nextInt(5,20) * 7);
+                String ageResult = String.valueOf(MathUtils.add(person.getAge(),b));
+                emailMessenger.sendMessage(ageResult);
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+
+        }
+
     }
 }
